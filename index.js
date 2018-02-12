a76d53298b8af77db98ca9a914370a177f771f64
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-12 15:35:08 +0700

    Load ENV

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+import DotENV from 'dotenv'
 import Express from 'express'
 import Http from 'http'
+import IP from 'ip'
 import SocketIO from 'socket.io'
+
+DotENV.config()
 
 const app = Express()
 const server = Http.Server(app)
 const io = SocketIO(server)
+const IPAddress = IP.address()
+const PORT = process.env.LOGGING_PORT
 
 io.on('connection', socket => socket.on('LOG', msg => console.log(msg)))
 
-server.listen(1000, err => err? console.log(err):console.log('Logging Server is ready at PORT 1000'))
+server.listen(PORT, err => err? console.log(err):console.log(`Logging Server is ready at ${IPAddress}:${PORT}`))
